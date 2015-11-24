fae2beffc6eefa1406c34acfeae3c0637d3d8114
adityasan92 <adityasan92@gmail.com>
2015-11-23 23:27:13 -0500

    removed the error creating part in the project

---
+++ src/BST.java
@@ -0,0 +1,18 @@
+
+public class BST extends BinarySearchTree{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void minTree(int[] arr){
+		
+		int length = arr.length; 
+		
+		//minTree(arr,0,length -1); 
+	
+	}
+
+
+}
