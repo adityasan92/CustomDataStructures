ebf7d6801d39625634df1f46014e22531f27ceaa
adityasan92 <adityasan92@gmail.com>
2015-12-03 22:24:36 -0500

    Added a print method and some test cases

--- src/QueueLinkedList.java
+++ src/QueueLinkedList.java
@@ -4,7 +4,16 @@ public class QueueLinkedList<E> {
 	private Node first, last; 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		QueueLinkedList<Integer> queue = new QueueLinkedList<Integer>();
+		queue.enqueue(4);
+		queue.enqueue(6);
+		queue.enqueue(7);
+		queue.enqueue(8);
+		queue.enqueue(9);
+		queue.enqueue(10);
+		queue.dequeue();
+		queue.print();
+		
 	}
 	
 	public boolean isEmpty(){
@@ -28,6 +37,14 @@ public class QueueLinkedList<E> {
 		first = first.next; 
 		if(isEmpty()) last = null;
 		return item; 
+	}
+	
+	public void print(){
+		Node temp = first; 
+		while(temp != null){
+			System.out.println(temp.item);
+			temp = temp.next; 
+		}
 	}
 	
 	private class Node{
